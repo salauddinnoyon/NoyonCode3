c5a138b06ec6a1f588da87e2b70ab4791ba0e1c9
Salauddin Noyon <sallu@Salauddins-MacBook-Pro.local>
2023-10-18 04:27:10 -0400

    New Project For Git Practice

---
+++ Git_Practice/src/test/java/com/bit/test/Git_Practice1.java
@@ -0,0 +1,24 @@
+package com.bit.test;
+
+public class Git_Practice1 {
+
+	public void m1() {
+		System.out.println("Not gonna give up");
+	}
+
+	public void m2() {
+
+		System.out.println("Going to break the code");
+
+	}
+
+	public void m3() {
+
+		System.out.println("I will get this job");
+	}
+
+	public void m4() {
+
+	}
+
+}
